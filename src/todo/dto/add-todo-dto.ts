e42fe9a1e690089f5a5fab13e011e36f4587aae6
Achref <medachrefg@gmail.com>
2022-09-24 17:14:39 +0100

    using class validator

--- src/todo/dto/add-todo-dto.ts
+++ src/todo/dto/add-todo-dto.ts
@@ -1,5 +1,15 @@
+import { IsString, MinLength, IsNotEmpty } from "class-validator";
+
+
 export class AddTodoDto {
+    @IsString()
+    @IsNotEmpty()
+    @MinLength(6, {
+        message: 'the filed need to have 6 carac min'
+    })
     name: string;
+    @IsString()
+    @IsNotEmpty()
     description: string;
 }
 
--- src/todo/dto/get-allTodo-dto.ts
+++ src/todo/dto/get-allTodo-dto.ts
@@ -1,4 +1,11 @@
+import { IsNumber, IsOptional } from "class-validator";
+
 export class getAllTodoDto {
+
+    @IsNumber()
+    @IsOptional()
     page: number;
+    @IsNumber()
+    @IsOptional()
     item: number;
 }
