5a1d41d2f6e9884fd43b1c063b7ae30de7e5422b
Achref <medachrefg@gmail.com>
2022-09-25 02:43:49 +0100

    configure our DB part 1

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,21 @@ import { FirsMiddleware } from './middlewares/first.middleware';
 import { logger } from './middlewares/logger.middleware';
 import { TodoModule } from './todo/todo.module';
 import { ConfigModule } from '@nestjs/config';
-
+import { TypeOrmModule } from '@nestjs/typeorm';
+import * as dotenv from 'dotenv';
+dotenv.config();
 @Module({
   imports: [TodoModule,
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_USERNAME,
+      entities: [],
+      synchronize: true,
+    }),
     ConfigModule.forRoot({ isGlobal: true })
   ],
   controllers: [AppController],
